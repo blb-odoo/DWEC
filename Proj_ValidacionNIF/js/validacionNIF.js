3bf9044b0321268ec50ba8484a3945c553cca0c5
Belen LB <blaserna@iessonferrer.net>
2023-01-24 19:07:58 +0100

    20230124 - Validar email

--- Proj_ValidacionNIF/js/validacionNIF.js
+++ Proj_ValidacionNIF/js/validacionNIF.js
@@ -13,9 +13,23 @@ function validarNombre() {
 /**
  * Función para validar el email del formulario
  */
+function validarEmail() {
+    let patron = /^.+@.+$/;
 
+    this.className = "";
+    if (patron.test(this.value)) {
+        this.className = "verde";
+    }
+}
+
+// Ejecución de la aplicación
 window.addEventListener('load', function(){
     let nombre = document.getElementById('nombre');
+    let apellidos = document.getElementById('apellidos');
+    let email = document.getElementById('email');
 
     nombre.addEventListener('keyup', validarNombre);
+    apellidos.addEventListener('keyup', validarNombre);
+    email.addEventListener('keyup', validarEmail);
+    
 });
