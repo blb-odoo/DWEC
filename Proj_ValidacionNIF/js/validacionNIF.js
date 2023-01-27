78919f9d36701234c622e0b9ad997dae38e825b1
Belen LB <blaserna@iessonferrer.net>
2023-01-27 19:37:16 +0100

    20230127 función validarDatos con  tfno nacional

--- Proj_ValidacionNIF/js/validacionNIF.js
+++ Proj_ValidacionNIF/js/validacionNIF.js
@@ -22,14 +22,32 @@ function validarEmail() {
     }
 }
 
+/**
+ * Función para validar varios patrones
+ */
+function validarDatos(){
+    let patrones = new Map();
+
+    patrones.set("nombre", /^[A-Za-záéíóúüàèiòÁÉÍÓÚÀÈÒÜñÑçÇ]{2,}$/);
+    patrones.set("apellidos", /^[A-Za-záéíóúüàèiòÁÉÍÓÚÀÈÒÜñÑçÇ]{2,}$/);
+    patrones.set("email", /^.+@.+$/);
+    patrones.set("telefonoNacional", /^([89][^09]|[67][0-9])[0-9]{7}$/);
+    
+    this.className = "";
+    if (patrones.get(this.id).test(this.value)) {
+        this.className = "verde";
+    }
+
+} 
+
 // Ejecución de la aplicación
-window.addEventListener('load', function(){
+    window.addEventListener('load', function(){
     let nombre = document.getElementById('nombre');
     let apellidos = document.getElementById('apellidos');
     let email = document.getElementById('email');
 
-    nombre.addEventListener('keyup', validarNombre);
-    apellidos.addEventListener('keyup', validarNombre);
-    email.addEventListener('keyup', validarEmail);
+    nombre.addEventListener('keyup', validarDatos);
+    apellidos.addEventListener('keyup', validarDatos);
+    email.addEventListener('keyup', validarDatos);
     
 });
