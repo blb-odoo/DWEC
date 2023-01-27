f2f9df0c0b18884746e04e361acfaaf92ac73f22
Belen LB <blaserna@iessonferrer.net>
2023-01-27 21:00:12 +0100

    20230127 patrones tarjetas

--- Proj_ValidacionNIF/js/validacionNIF.js
+++ Proj_ValidacionNIF/js/validacionNIF.js
@@ -11,6 +11,15 @@ function validarDatos(){
     patrones.set("telefonoNacional", /^([89][^09]|[67][0-9])[0-9]{7}$/);
     patrones.set("telefonoConPrefijo", /^\(\+[0-9]{1,3}\)([89][^09]|[67][0-9])[0-9]{7}$/);
     patrones.set("fecha", /^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}$/)
+    patrones.set("codigoPostal", /^(0[1-9]|[1-4][0-9]|5[0-2])[0-9]{3}$/);
+    patrones.set("tiempo", /^([0-1][0-9]|2[0-3])(:([0-5][0-9])){2}$/);
+
+    let patronVisa = /^4([0-9]{12}|[0-9]{15})$/;
+    let patronMasterCard = /^5[1-5][0-9]{14}$/;
+    let patronDiscover = /^(6011[0-9]{12}|5[0-9]{14})$/;
+    let patronAmericanExpress =/^(34|37)[0-9]{13}$/;
+    let patronDinersClub = /^(30[0-5][0-9]{11}|(36|38)[0-9]{12})$/;
+
 
     this.className = "";
     if (patrones.get(this.id).test(this.value)) {
